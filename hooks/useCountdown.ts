5207e9747f28f54e4513c78ba20ba792049532c4
Ian Duffy <ian.duffy@shiftcommerce.com>
2022-08-13 19:59:42 +0100

    basics of useCountdown

---
+++ hooks/useCountdown.ts
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react'
+
+interface TimeLeft {
+  days: number | '00'
+  hours: number | '00'
+  minutes: number | '00'
+  seconds: number | '00'
+}
+
+const getDays = (difference: Number) => Math.floor(difference / (1000 * 60 * 60 * 24))
+
+const getHours = (difference: Number) => Math.floor((difference / (1000 * 60 * 60)) % 24)
+
+const getMinutes = (difference: Number) => Math.floor((difference / 1000 / 60) % 60)
+
+const getSeconds = (difference: Number) => Math.floor((difference / 1000) % 60)
+
+const useCountDown = (date: string) => {
+  const [timeLeft, setTimeLeft] = useState<TimeLeft>({
+    days: '00',
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  })
+
+  const calculateTimeLeft = () => {
+    const difference : Number = Number(new Date(date)) - Number(new Date());
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: getDays(difference),
+        hours: getHours(difference),
+        minutes: getMinutes(difference),
+        seconds: getSeconds(difference),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  useEffect(() => {
+    setTimeLeft(calculateTimeLeft())
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft())
+    }, 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  return timeLeft;
+}
+
+export default useCountDown
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import useCountDown from '../hooks/useCountdown'
 
 const Home: NextPage = () => {
+  const { days, hours, minutes, seconds } = useCountDown('10-13-2022')
+
   return (
     <h1 className="text-7xl font-bold underline">
-      Hello world!
+      {days}:{hours}:{minutes}:{seconds}
     </h1>
   )
 }
